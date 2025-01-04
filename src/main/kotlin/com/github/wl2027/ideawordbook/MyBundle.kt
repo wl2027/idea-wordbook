907c52a0c24beed697941746959982a51506d5b8
GitHub Action <action@github.com>
2025-01-04 11:48:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/wl2027/ideawordbook/MyBundle.kt
+++ src/main/kotlin/com/github/wl2027/ideawordbook/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.wl2027.ideawordbook
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/wl2027/ideawordbook/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/wl2027/ideawordbook/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wl2027.ideawordbook.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
